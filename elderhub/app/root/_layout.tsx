bc1f3212371470ea7d3596a34572f844a86f2cea
Dipesh <mavinash423@gmail.com>
2025-03-20 21:46:00 +0530

    shree ganesh

---
+++ elderhub/app/root/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="signin" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
